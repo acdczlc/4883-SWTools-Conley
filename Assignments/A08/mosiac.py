f848c3d403d7e54a267c0d044ff2de6a4842e23c
acdczlc <acdczlc@gmail.com>
2019-03-31 20:35:30 -0500

    Added A08

---
+++ Assignments/A08/mosiac.py
@@ -0,0 +1,120 @@
+#Zac Conley
+# A08-Mosiac-Mosiac.py
+#This program creates a mosiac of a pic using emojis
+import os
+import sys
+import requests #hits api
+import json
+from difflib import SequenceMatcher #used to guess best color
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
+
+#finds the closest emoji to the current color
+#accepts a color json file and the input folder name
+#returns path to an emoji
+def find_emoji(color):
+    emoji="" #matched emoji
+    match=0 
+    emoji2="" #matched emoji
+    match2=0
+    color1=""
+    dist1=100 #distance between colors
+    color2=""
+    dist2=100
+    bestguess="" #guess based on string matching
+    guess_color=1 
+    with open('processed_emojis.json') as f:
+        data = json.load(f)
+    for result in color["result"]:
+        if(result["dist"]<dist1):
+            dist2=dist1 #saves distance to second color before overwriting with new
+            color2=color1 #closer color
+            dist1=result["dist"] #overwrite
+            color1=result["name"]
+        elif(result["dist"]<dist2):
+            dist2=result["dist"]
+            color2=result["name"]
+    for image,values in data.items():
+        for item in values:
+            for colors,value in item.items():
+                if(colors == color1 and value>match): #emoji contains closer match
+                    emoji=image #save emoji
+                    match=value #save percentage
+                elif(colors == color2 and value>match2):
+                    emoji2=image
+                    match2=value    
+                elif(emoji=="" and emoji2==""): #if there is no exact match, takes a guess based on name
+                    guess_per= SequenceMatcher(None, colors, color1).ratio()
+                    if(guess_per<guess_color):
+                        bestguess=image
+                        guess_color=guess_per
+                    guess_per=SequenceMatcher(None, colors, color2).ratio()
+                    if(guess_per<guess_color):
+                        bestguess=image
+                        guess_color=guess_per 
+    if(emoji=="" and emoji2==""): #if there is no exact match, take a guess
+        return bestguess
+    else:
+        fin_match=max(match,match2) #return exact match
+        if(fin_match==match):
+            return emoji
+        elif(fin_match==match2):
+            return emoji2
+
+if __name__=='__main__':
+    print("\nZac Conley")
+    print("A08-Mosiac-Mosiac.py")
+    print("This program creates a mosiac of a pic using emojis \n")
+    print("This will take a while!")
+    print("if you wish to speed up the process you can increase the chunk size to decrease the quality!")
+    args = {}
+    output="" #no output default
+    image="hurricane.png" #default image
+    folder="emojis" #default folder
+    emoji_size=16 #default 16 emoji size
+    chunk_size=4
+    for arg in sys.argv[1:]: # get arguments from command line
+        f_type,value = arg.split('=')
+        if(f_type=="image"):
+            image=value
+        elif(f_type=="input_folder"):
+            folder=value
+        elif(f_type=="size"):
+            chunk_size=int(value)
+        elif(f_type=="output_folder"):
+            output=value
+    try:
+        newimg = Image.open(image) #opens the image
+        newimg=newimg.convert('RGB') #get rid of alpha channel
+        width,height = newimg.size #save dimensions
+        pix=list(newimg.getdata()) # list of all pixels
+        newimg=newimg.convert('RGBA').resize((width*emoji_size,height*emoji_size))
+        count=0 #current pixel in pic
+        x=0 #width
+        y=0 #height
+        while(y<height):
+            while(x<width):
+                pixel_rgb = {'r':pix[count][0], 'g':pix[count][1], 'b':pix[count][2],'d':3} # get current pixel
+                pixel_color = requests.get('http://cs.mwsu.edu/~griffin/color-api/', params=pixel_rgb) #hit api
+                emoji=find_emoji(pixel_color.json()) #find closest emoji
+                emoji=folder+"/"+emoji
+                temp=emoji #save emoji so it can be used for whole chunk
+                chunk=0 
+                while(chunk<chunk_size): #foreach emoji in chunk paste the same emoji
+                    emoji=Image.open(temp)
+                    emoji=emoji.convert('RGBA').resize((emoji_size,emoji_size))
+                    newimg.paste(emoji,(x*emoji_size,y*emoji_size),emoji) #add emoji to pic
+                    if(x<width): #if using a large chunk size, this will prevent error
+                        count+=1
+                    x+=1 #increment width
+                    chunk+=1
+            x=0 #reset width
+            y+=1 #increment to next row
+        name,dot = image.split('.') #gets name of file and the post dot extension
+        if(not(os.path.isdir(output))):
+            f_name = name+"mosaic."+dot
+            newimg.save(f_name)
+        else:
+            newimg.save(output+"/"+name+"mosaic."+dot) #save mosiac in output folder
+        print("File saved!")
+    except:
+        print("An error has occurred with your input. Please check the github documentation")
---
+++ Assignments/A08/process_emojis.py
@@ -0,0 +1,94 @@
+#Zac Conley
+# A08-Mosiac-process_emojis.py
+#This program processes all emojis in a given folder
+import os
+import sys
+import cv2
+from PIL import Image,ImageMath
+import json
+import numpy as np
+import requests
+from sklearn.cluster import KMeans
+
+if __name__=='__main__':
+    print("\nZac Conley")
+    print("A08-Mosiac-process_emojis.py")
+    print("This program processes all emojis in a given folder \n")
+    args = {}
+    for arg in sys.argv[1:]: #split command line arguments
+        f_type,values = arg.split('=')
+        args[f_type] = values
+        if(f_type=='folder'):
+            emoji_folder=values
+    try:
+        if(os.path.isdir(emoji_folder)): #checks if emoji folder is a directory
+            l = os.listdir(emoji_folder) 
+            num_files=len(l) #gets length of emoji folder
+            image_vals={}
+            counter=0 #shows user which file is being processed
+            for name in l: # for each emoji in folder
+                counter+=1 #increment counter
+                print("Processing "+name+" file "+str(counter)+" of "+str(num_files))
+                img=(emoji_folder+'/'+name) #get current emoji
+                img = cv2.imread(img)
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                img = img.reshape((img.shape[0] * img.shape[1],3))
+                cluster = KMeans(n_clusters=3) #cluster emoji
+                cluster.fit(img)
+                lab = np.arange(0, len(np.unique(cluster.labels_)) + 1)
+                (h_gram, _) = np.histogram(cluster.labels_, bins=lab)
+                h_gram = h_gram.astype("float")
+                h_gram /= h_gram.sum()
+                list_colors = [] #list of main emoji colors
+                for (match, color) in zip(h_gram, cluster.cluster_centers_):
+                    rgb = [] #list of rgb
+                    for val in color:
+                        val = round(float(val))
+                        rgb.append(val)
+                    list_colors.append({'percent':round(float(match),2),'rgb':rgb})
+                for i in range(len(list_colors)):
+                    val = [] #holds clusters
+                    delta = 3 
+                    while len(val) < 1: #hit api to get most colors
+                        emoji_color_cluster = {'r':list_colors[i]['rgb'][0], 'g':list_colors[i]['rgb'][1], 'b':list_colors[i]['rgb'][2],'d':delta}
+                        r = requests.get('http://cs.mwsu.edu/~griffin/color-api/', params=emoji_color_cluster)
+                        val=r.json() #save colors
+                        delta += 3 
+                    list_colors[i]['named_data'] = val #save colors
+                p=list_colors[0]["percent"]
+                dist=1
+                for color in list_colors[0]["named_data"]["result"]: #get names of colors and percentages
+                    if(color["dist"]<dist):
+                        dist=color["dist"]
+                        c_name=color["name"]
+                image_vals[name]=[]
+                image_vals[name].append({c_name:p})
+                p1=list_colors[1]["percent"]
+                dist=1
+                for color in list_colors[1]["named_data"]["result"]: #get names of colors and percentages
+                    if(color["dist"]<dist):
+                        dist=color["dist"]
+                        c_name=color["name"]
+                if(p>p1):
+                    image_vals[name].insert(1,{c_name:p1})
+                else:
+                    image_vals[name].insert(0,{c_name:p1})
+                p2=list_colors[2]["percent"]
+                dist=1
+                for color in list_colors[2]["named_data"]["result"]: #get names of colors and percentages
+                    if(color["dist"]<dist):
+                        dist=color["dist"]
+                        c_name=color["name"]
+                if(p>p2 and p1>p2):
+                    image_vals[name].insert(2,{c_name:p2})
+                elif(p2>p and p2>p1):
+                    image_vals[name].insert(0,{c_name:p2})
+                else:
+                    image_vals[name].insert(1,{c_name:p2})
+            f=open("processed_emojis.json",'w') #open new file to write dict to
+            f.write(json.dumps(image_vals)) #write dict to
+            f.close() 
+            print("All emojis have been processed!") #success message    
+            print("A new file has been created with all the info.")                    
+    except: # error message
+        print("An error has occured, check my GitHub documentation for command line specifications")
